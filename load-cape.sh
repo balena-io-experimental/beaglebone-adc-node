1340f01b7bb9d6f822cb241d85101d95a3fefd7b
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-10-01 16:25:44 +0100

    switch from cape-universaln to cape-universal

--- load-cape.sh
+++ load-cape.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #load the universal BBB cape using capemgr, so we have access to ADC device.
-echo cape-universaln > /sys/devices/platform/bone_capemgr/slots
+echo cape-universal > /sys/devices/platform/bone_capemgr/slots
